dc7e8c2d3890982d2b481469be4805641bb9e207
sam <samcodesit@gmail.com>
2021-11-01 20:23:48 +0000

    Tweak appveyor

--- update_submodule.sh
+++ update_submodule.sh
@@ -2,17 +2,3 @@
 
 # This script gets the latest changes from the submodule, commits the changes, and pushes to the remote (only if there are any changes, to avoid an infinite build loop)
 
-git submodule update --init --recursive
-git submodule update --remote --merge
-
-if [[ `git status --porcelain` ]]; then
-  # Push the latest version of the Geometrize Twitter bot submodule to the repository
-  git config --global user.email "appveyor@appveyor.org"
-  git config --global user.name "AppVeyor CI Bot"
-  git checkout master
-  git add geometrize-twitter-bot
-  git commit --message "Build bot updating submodule and rebuilding documentation"
-  git remote rm origin
-  git remote add origin https://${GITHUB_API_KEY}@github.com/Tw1ddle/geometrize-twitter-bot-docs.git > /dev/null 2>&1
-  git push origin master
-fi

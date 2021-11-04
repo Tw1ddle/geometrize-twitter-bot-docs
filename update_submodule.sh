2c2903cc7f6c1d1dc10eaaa852b92b609de04db4
sam <samcodesit@gmail.com>
2021-11-04 21:32:54 +0000

    Update api key, remove redundant script

--- update_submodule.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script gets the latest changes from the submodule, commits the changes, and pushes to the remote (only if there are any changes, to avoid an infinite build loop)
-
